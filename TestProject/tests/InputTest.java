20ffd0fb42dc12fd540854335caccfab1209353c
LodeWouters <49675096+LodeWouters@users.noreply.github.com>
2019-04-26 07:58:34 +0200

    setting changes

--- TestProject/tests/InputTest.java
+++ TestProject/tests/InputTest.java
@@ -7,17 +7,18 @@ import org.junit.Test;
 import model.Password;
 import model.Username;
 
+
 public class InputTest {
 
 	@Test
-	public void checkUsername() {
+	public void testCheckUsername() {
 		Username username = new Username();
 		username.setUsername("test");
 		assertEquals("test", username.getUsername());
 	}
 	
 	@Test
-	public void checkPassword() {
+	public void testCheckPassword() {
 		Password password = new Password();
 		password.setPassword("testpwd");
 		assertEquals("testpwd", password.getPassword());
