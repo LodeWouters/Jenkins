f22da84b3b2ff78b077b106f6add9d8984203190
LodeWouters <49675096+LodeWouters@users.noreply.github.com>
2019-04-29 12:07:49 +0200

    added JUnit4Test

--- TestProject/tests/JUnit4Test.java
+++ TestProject/tests/JUnit4Test.java
@@ -1,14 +1,13 @@
 package tests;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 import model.Password;
 import model.Username;
 
-class JupiterTest {
+public class JUnit4Test {
 
 	@Test
 	public void testCheckUsername() {
