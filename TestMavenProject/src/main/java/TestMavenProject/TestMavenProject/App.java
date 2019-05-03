a30bed1a518d9aaf4e8384fac9c680aa3edaade7
LodeWouters <49675096+LodeWouters@users.noreply.github.com>
2019-05-03 14:13:52 +0200

    Update App.java

--- TestMavenProject/src/main/java/TestMavenProject/TestMavenProject/App.java
+++ TestMavenProject/src/main/java/TestMavenProject/TestMavenProject/App.java
@@ -2,20 +2,34 @@ package TestMavenProject.TestMavenProject;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 
 public class App 
 {
 		public static void main(String[] args) {
-			List<Pair<Username, Password>> passwords = new ArrayList<Pair<Username,Password>>();
-
-			passwords.add(new Pair("Testname","testpwd"));
-			System.out.println("Username: " + passwords.get(0).getUsername());
-			System.out.println("Password: " + passwords.get(0).getPassword());
+			List<Pair<Username, Password>> pairs = new ArrayList<Pair<Username,Password>>();
+			Scanner scanner = new Scanner(System.in);
+			System.out.println("Enter username: ");
+			Username username = new Username();
+			username.setUsername(scanner.nextLine());
+			System.out.println("Enter password: ");
+			Password password = new Password();
+			password.setPassword(scanner.nextLine());
 			
-			Username enteredName = new Username("Newname");
-			passwords.get(0).setUsername(enteredName);
-			System.out.println("Your new username is: " + enteredName.getUsername());
+			pairs.add(new Pair(username, password));
+
+			System.out.println("Username: " + pairs.get(0).getUsername().getUsername());
+			System.out.println("Password: " + pairs.get(0).getPassword().getPassword());
+			
+			pairs.add(new Pair(new Username("username2"), new Password("password2")));
+			
+			System.out.println("\nUsername 2: " + pairs.get(1).getUsername().getUsername() + "\nPassword 2: " + pairs.get(1).getPassword().getPassword());
+			
+			System.out.println("\nList of pairs: ");
+			for(int i = 0; i < pairs.size(); i++) {
+				
+			System.out.println(pairs.get(i) + " ");
+			}
 	}
-		
 }
